cc763fd8e10f3698fd1ebbfd9dc1092398baedfb
Giovanni Paradisi <giovanni.paradisi2@studio.unibo.it>
2026-03-26 12:21:25 +0100

    feat: ex complex done

--- src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
@@ -5,11 +5,11 @@ package it.unibo.pps.tasks.adts
  *  the test in ComplexTest.
  */
 
-object Ex1ComplexNumbers:
+object Ex1ComplexNumber:
 
   trait ComplexADT:
     type Complex
-    def complex(re: Double, im: Double): Complex
+    def toComplex(re: Double, im: Double): Complex
     extension (complex: Complex)
       def re(): Double
       def im(): Double
@@ -20,11 +20,15 @@ object Ex1ComplexNumbers:
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    case class ComplexImpl(re: Double, im:Double)
+    type Complex = ComplexImpl
+    def toComplex(re: Double, im: Double): Complex = ComplexImpl(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.re
+      def im(): Double = complex.im
+      def sum(other: Complex): Complex = toComplex(complex.re() + other.re(), complex.im() + other.im())
+      def subtract(other: Complex): Complex = toComplex(complex.re() - other.re(), complex.im() - other.im())
+      def asString(): String = (re(), im()) match
+        case (re, 0) => s"${complex.re()}"
+        case (0, im) => s"${complex.im()}i"
+        case (re, im) => if im > 0 then s"${complex.re()} + ${complex.im()}i" else s"${complex.re()} - ${Math.abs(complex.im())}i"
--- src/test/scala/it/unibo/pps/tasks/adts/ComplexNumberTests.scala
+++ src/test/scala/it/unibo/pps/tasks/adts/ComplexNumberTests.scala
@@ -2,38 +2,38 @@ package it.unibo.pps.tasks.adts
 
 import org.junit.*
 import org.junit.Assert.*
-import Ex1ComplexNumbers.*
+import Ex1ComplexNumber.*
 
 /* Tests should be clear, but note they are expressed independently of the 
    specific implementation  -- UNCOMMENT FOR THE EXERCISE!
 */
-class ComplexTest:
+class ComplexNumberTests:
 
   // Choice of implementation to test
-  val complexADT: ComplexADT = BasicComplexADT
-  import complexADT.*
+  val toComplexADT: ComplexADT = BasicComplexADT
+  import toComplexADT.*
 
   // From now, everything is independent of specific implementation of Complex
 
   @Test def testReal() =
-    assertEquals(10, complex(10, 20).re(), 0)
+    assertEquals(10, toComplex(10, 20).re(), 0)
 
   @Test def testImaginary() =
-    assertEquals(20, complex(10, 20).im(), 0)
+    assertEquals(20, toComplex(10, 20).im(), 0)
 
   @Test def testSum() =
-    assertEquals(complex(11, 22), complex(10, 20) sum complex(1, 2))
+    assertEquals(toComplex(11, 22), toComplex(10, 20) sum toComplex(1, 2))
 
   @Test def testSubtract() =
-    assertEquals(complex(9, 18), complex(10, 20) subtract complex(1, 2))
+    assertEquals(toComplex(9, 18), toComplex(10, 20) subtract toComplex(1, 2))
 
   @Test def testAsString() =
-    assertEquals("10.0 + 5.0i", complex(10.0, 5.0).asString())
+    assertEquals("10.0 + 5.0i", toComplex(10.0, 5.0).asString())
 
   @Test def optionalTestAdvancedAsString() =
-    assertEquals("0.0", complex(0, 0).asString())
-    assertEquals("10.0", complex(10.0, 0).asString())
-    assertEquals("10.0 + 5.0i", complex(10.0, 5.0).asString())
-    assertEquals("10.0 - 5.0i", complex(10.0, -5.0).asString())
-    assertEquals("5.0i", complex(0, 5.0).asString())
-    assertEquals("-5.0i", complex(0, -5.0).asString())
+    assertEquals("0.0", toComplex(0, 0).asString())
+    assertEquals("10.0", toComplex(10.0, 0).asString())
+    assertEquals("10.0 + 5.0i", toComplex(10.0, 5.0).asString())
+    assertEquals("10.0 - 5.0i", toComplex(10.0, -5.0).asString())
+    assertEquals("5.0i", toComplex(0, 5.0).asString())
+    assertEquals("-5.0i", toComplex(0, -5.0).asString())
