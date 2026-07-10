337360fef181fbcfda1f7d3442b53da1558273c4
Giovanni Paradisi <giovanni.paradisi2@studio.unibo.it>
2026-07-10 14:27:34 +0200

    feat: done the Ex5 Traversable

--- src/main/scala/it/unibo/pps/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/it/unibo/pps/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,8 @@
 package it.unibo.pps.tasks.typeclasses
 
-import it.unibo.pps.u03.Sequences.Sequence, Sequence.*
+import it.unibo.pps.u03.Sequences.Sequence
+import Sequence.*
+import it.unibo.pps.u03.Optionals.Optional
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -23,4 +25,32 @@ object Ex5Traversable:
     case Cons(h, t) => log(h); logAll(t)
     case _ => ()
 
-  
+  trait Traversable[T[_]]:
+    def traverse[A](t: T[A])(f: A => Unit): Unit
+
+  def logAll[T[_], A](t: T[A])(using tr: Traversable[T]): Unit =
+    tr.traverse(t)(log)
+
+  given Traversable[Optional] with
+    def traverse[A](opt: Optional[A])(f: A => Unit): Unit = opt match
+      case Optional.Just(a) => f(a)
+      case Optional.Empty() => ()
+
+  given Traversable[Sequence] with
+    def traverse[A](seq: Sequence[A])(f: A => Unit): Unit = seq match
+      case Cons(h, t) => f(h); traverse(t)(f)
+      case _ => ()
+
+@main def tryTraversable =
+  import it.unibo.pps.tasks.typeclasses.Ex5Traversable.*
+
+  val opt = Optional.Just(1)
+  logAll(opt) // The next element is: 1
+
+  val seq = Cons(10.0, Cons(20.0, Cons(30.0, Nil())))
+  logAll(seq) // The next element is: 10.0
+              // The next element is: 20.0
+              // The next element is: 30.0
+
+  val nn = Optional.Empty()
+  logAll(nn) // "nothing"
